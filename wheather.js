dd0384a19c3c35eec6e7e197a10a881c06e4876a
Sean Brown <seanbrhn3@gmail.com>
2017-01-28 16:24:12 -0500

    finishing js code

--- wheather.js
+++ wheather.js
@@ -1,14 +1,18 @@
 var fs = require('fs');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var xhr = new XMLHttpRequest();
-xhr.open("GET","http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=7dd3cabf117ddc729b666e5b23c24676",false);
-xhr.send();
 var contents = fs.readFileSync("city.list.us.json");
 var jscon = JSON.stringify(contents);
+var id = "";
 function grabState(state){
-	if(jscon.name == state){
-		console.log("this works!");
+	for(cid in jscon){
+		if(jscon.name == state){
+			id = jscon[cid].id;
+		}
 	}
 }
 
-console.log(grabState("Bel Air"));
+grabState("Jones Crossroads");
+
+xhr.open("GET","http://api.openweathermap.org/data/2.5/forecast/city?" + id + "&APPID=7dd3cabf117ddc729b666e5b23c24676",false);
+xhr.send();
