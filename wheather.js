8288dbba495b4e2531d3c32f7d2720a3ac998213
Sean Brown <seanbrhn3@gmail.com>
2017-01-29 02:19:22 -0500

    accuqheather

--- wheather.js
+++ wheather.js
@@ -1,18 +1,12 @@
 var fs = require('fs');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+var http = require('http');
+var request = require('request');
+var keepAlive = new http.Agent({keepAlive:true});
 var contents = fs.readFileSync("city.list.us.json");
 var jscon = JSON.stringify(contents);
-var id = "";
-function grabState(state){
-	for(cid in jscon){
-		if(jscon.name == state){
-			id = jscon[cid].id;
-		}
-	}
-}
+var id = 0;
+var request = "";
+var jsonText;
+var answer = "";
+//this function creates the request based on the number given
 
-grabState("Jones Crossroads");
-
-xhr.open("GET","http://api.openweathermap.org/data/2.5/forecast/city?" + id + "&APPID=7dd3cabf117ddc729b666e5b23c24676",false);
-xhr.send();
